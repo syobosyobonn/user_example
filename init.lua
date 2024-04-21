840f566358047342b5df5bb6bb5133475677ad88
Syobosyobonn <154320513+syobosyobonn@users.noreply.github.com>
2024-04-21 21:36:40 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -82,4 +82,13 @@ return {
     --   },
     -- }
   end,
+  vim.api.nvim_create_autocmd("VimEnter", {
+  command = "Neotree toggle",
+})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',  
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+  end
+})
 }
